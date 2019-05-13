d350949e15953f95109bfaa4786c6c22da13c6ba
Adam Wolfman <adamwolfman@Adams-MacBook-Pro.local>
2019-05-13 13:57:06 -0600

    timer buttons added

--- front-end/src/WorkoutContainer/WorkoutList/TimerApp/TimerApp.jsx
+++ front-end/src/WorkoutContainer/WorkoutList/TimerApp/TimerApp.jsx
@@ -4,14 +4,35 @@ class TimerApp extends Component {
     constructor(){
         super();
         this.state = {
-
+            seconds: 0,
+            intervalOne: 0,
+            intervalTwo: 0,
+            cycles: 0
         }
+    
     }
 
+    componentDidMount = () => {
+        this.setState({
+            seconds: 0,
+            intervalOne: this.props.workout.intervalOne,
+            intervalTwo: this.props.workout.intervalTwo,
+            cycles: this.props.workout.cycles
+        })
+    }
+
+
+
     render(){
+        console.log(this.state, "state in timer component")
         return(
             <div>
-                <h1>{this.props.workout.name}</h1>
+
+                <div>
+                    <div className='timer'></div>
+                    <button className='start-button'>Start</button>
+                    <button className='stop-button'>Stop</button>
+                </div>
             </div>
         )
     }
